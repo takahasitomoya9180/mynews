eafb503bee0c3543b57e0463bad40ce25d272c46
tomoya <t0819t@ezweb.ne.jp>
2019-05-01 16:03:05 +0000

    .課題

---
+++ kadai.php
@@ -0,0 +1,34 @@
+<?php
+$a=3;
+$b=7;
+echo $a+$b;
+
+$array_month=['㋀','㋁','㋂','㋃','㋄','㋅','㋆','㋇','㋈','㋉','㋊','㋋'];
+echo $array_month[7];
+
+
+$name="tomoya";
+$world="'s World!";
+echo $hello.$name.$world;
+
+
+$tech_boost="tech";
+$tech_boost.=" boost";
+echo $tech_boost;
+
+$y2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+echo $y2018_calendar["December"];
