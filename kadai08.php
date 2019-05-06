d31a0c6e69412c912fb782abb0809936c694e2b7
tomoya <t0819t@ezweb.ne.jp>
2019-05-06 14:14:36 +0000

    kadai08

---
+++ kadai08.php
@@ -0,0 +1,30 @@
+<?php
+
+//prfilecontroller.php作成時のコードです
+//php artisan make:controller Admin/ProfileController
+namespace App\Http\Controllers\Admin;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class profileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    public function create()
+    {
+        return view('admin/profile/create');
+    }
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    public function update()
+    {
+        return view('admin/profile/edit');
+    }
+}
