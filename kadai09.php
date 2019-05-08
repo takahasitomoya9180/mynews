568a4d32c25e0316c48c97d644524562c8f97949
tomoya <t0819t@ezweb.ne.jp>
2019-05-08 13:42:02 +0000

    kadai09

---
+++ kadai09.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//課題３「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定です
+Route::group(["prefix"=>"xxx"],function(){
+    Route::get('xxx','AAA/Controller@bbb');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//課題４
+Route::group(['prefix' => 'admin'],function(){
+     Route::get('plofile/create','admin/plofilecontroller@add');
+     Route::get('plofile/edit','admin/plofilecontroller@edit');
+     
+} );
