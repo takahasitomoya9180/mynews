c7a500ea1682c0f0205fdad367250b0b844b512f
tomoya <t0819t@ezweb.ne.jp>
2019-05-17 05:18:59 +0000

    kadai11

---
+++ storage/framework/views/ca95b00855bc32568fe571b35549debb2c197cc3.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+
+
+
+
+<?php $__env->startSection('title','My Profile'); ?>
+
+
+<?php $__env->startSection('content'); ?>
+     <div class="container">
+         <div class="row">
+             <div class="col-md-8 mx-auto">
+                 <h2>おっさんのプロフとかワロスwwwww m9(^Д^)プギャーwww</h2>
+                 <p>HIITがダイエットに良い３つのメリットについて</p>
+                 <ol>
+                     <li>短時間の運動で脂肪が燃焼される</li>
+                     <li>アフターバーン効果がある（なんと運動後の数時間も脂肪燃焼効果が続く！)</li>
+                     <li>全身の大きな筋肉をメインで鍛えるので基礎代謝が上がる</li>
+                 </0l>
+                 <dr>
+                     <P>
+                    以上がHIITのメリットになりますが特に３つ目の「基礎代謝が上がる」というのが重要！<br>
+                     
+                 
+                 人は何もしてない時もカロリーを消費していてこれを「基礎代謝」といいます。<br>
+                 基礎代謝が高ければ高いほど、運動をしていなくても消費されるカロリーが増えるわけですね（最高かよ）<br>
+                 
+                 では、なぜ大きな筋肉を鍛えると基礎代謝が上がるのか？<br>
+                 
+                 実は筋肉が増えると、それを維持する為にカロリーを消費します。<br>
+                 つまり、筋トレして筋肉が増えれば増えるほど、何もしてなくても消費するカロリーが増えるのです（基礎代謝尊い）<br>
+                 HIITはとくに基礎代謝が上がる大きな筋肉<br>
+                 「胸筋・大腿四頭筋（身体の中で一番大きい太ももの筋肉）・広背筋など」
+                 をメインに鍛えるトレーニングです。<br>
+                 トレーニング後のアフターバーン効果にさらに基礎代謝がブーストされ、そこに断食を組み合わせたら痩せないわけがないですね^^<br>
+                 これなら今からでも全然夏に間に合う！！<br>
+                 目指せ、ひと夏のアバンチュール☆（にじみ出る昭和感）
+                 
+        
+                    
+                 
+                 
+                 </P>
+                 
+                 
+                 
+                 
+                 
+                
+        
+                 
+                 
+             </div>
+         </div>
+     </div>
+     <?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.profile', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/ec2-user/environment/mynews/resources/views/admin/profile/create.blade.php ENDPATH**/ ?>
