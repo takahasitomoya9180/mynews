28deb222170913c45bbaee8148508682bf226766
tomoya <t0819t@ezweb.ne.jp>
2019-05-17 03:27:55 +0000

    kadai11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,57 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charcet="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む　--}}
+@extends('layouts.profile')
+
+
+{{-- admin.profile.phpの@yield('title')に'ニュースの新規作成'を埋め込む　--}}
+@section('title','My Profile')
+
+{{-- plofile.blade.phpの@yield('content')に以下のタグを埋め込む　--}}
+@section('content')
+     <div class="container">
+         <div class="row">
+             <div class="col-md-8 mx-auto">
+                 <h2>おっさんのプロフとかワロスwwwww m9(^Д^)プギャーwww</h2>
+                 <p>HIITがダイエットに良い３つのメリットについて</p>
+                 <ol>
+                     <li>短時間の運動で脂肪が燃焼される</li>
+                     <li>アフターバーン効果がある（なんと運動後の数時間も脂肪燃焼効果が続く！)</li>
+                     <li>全身の大きな筋肉をメインで鍛えるので基礎代謝が上がる</li>
+                 </0l>
+                 <dr>
+                     <P>
+                    以上がHIITのメリットになりますが特に３つ目の「基礎代謝が上がる」というのが重要！<br>
+                     
+                 
+                 人は何もしてない時もカロリーを消費していてこれを「基礎代謝」といいます。<br>
+                 基礎代謝が高ければ高いほど、運動をしていなくても消費されるカロリーが増えるわけですね（最高かよ）<br>
+                 
+                 では、なぜ大きな筋肉を鍛えると基礎代謝が上がるのか？<br>
+                 
+                 実は筋肉が増えると、それを維持する為にカロリーを消費します。<br>
+                 つまり、筋トレして筋肉が増えれば増えるほど、何もしてなくても消費するカロリーが増えるのです（基礎代謝尊い）<br>
+                 HIITはとくに基礎代謝が上がる大きな筋肉<br>
+                 「胸筋・大腿四頭筋（身体の中で一番大きい太ももの筋肉）・広背筋など」
+                 をメインに鍛えるトレーニングです。<br>
+                 トレーニング後のアフターバーン効果にさらに基礎代謝がブーストされ、そこに断食を組み合わせたら痩せないわけがないですね^^<br>
+                 これなら今からでも全然夏に間に合う！！<br>
+                 目指せ、ひと夏のアバンチュール☆（にじみ出る昭和感）
+                 
         
-        <title>リーンゲインズﾞ</title>
-    </head>
-     <body>
-        <h1>～リーンゲインズ～</h1>
-        <p>無理せず楽して痩せられる！</p>
-    </body>
-</html>
+                    
+                 
+                 
+                 </P>
+                 
+                 
+                 
+                 
+                 
+                
+        
+                 
+                 
+             </div>
+         </div>
+     </div>
+     @endsection
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -25,7 +25,7 @@
         {{-- Laravel標準で用意されているCSSを読み込みます --}}
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         {{-- この章の後半で作成するCSSを読み込みます --}}
-        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
