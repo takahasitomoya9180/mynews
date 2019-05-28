6872f289f79f231d729e6350f278900f243b5152
tomoya <t0819t@ezweb.ne.jp>
2019-05-28 12:13:19 +0000

    kadai

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,7 +8,7 @@
                 <h2>ニュース新規作成</h2>
                 <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
 
-                    @if (count($errors) > 0)
+                   @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
                                 <li>{{ $e }}</li>
