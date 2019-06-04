a29a80c060fd837e8605a45e2cf3833e2bd9d25e
tomoya <t0819t@ezweb.ne.jp>
2019-06-04 12:41:10 +0000

    kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
-    //
     public function add()
   {
       return view('admin.profile.create');
@@ -15,16 +16,17 @@ class ProfileController extends Controller
 
   public function create(Request $request)
   {
+      
+      $this->validate($request, profile::$rules);
+      
+      $profile = new profile;
+      $form = $request->all();
+      
+      unset($form['_token']);
+      
+      $profile->fill($form);
+      $profile->save();
+      
       return redirect('admin/profile/create');
   }
-
- public function edit()
- {
- return view('admin.profile.edit');
- }
- public function update()
- {
- return redirect('admin/profile/edit');
- }
 }
-
