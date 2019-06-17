31b46a49628b3915fa45bdfe191c8baf6296124f
tomoya <t0819t@ezweb.ne.jp>
2019-06-17 13:26:19 +0000

    kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,7 +16,7 @@ class ProfileController extends Controller
 
   public function create(Request $request)
   {
-      
+      // Varidationを行う
       $this->validate($request, profile::$rules);
       
       $profile = new profile;
@@ -29,4 +29,32 @@ class ProfileController extends Controller
       
       return redirect('admin/profile/create');
   }
+  
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+          abort(404);
+      }
+      return view('admin.profile.edit',['profile_form' => $profile]);
+  }
+  
+  public function update(Request $request)
+  {
+      // validationをかける
+      $this->validation($request, Profile::$rules);
+      //　Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      //送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+      
+      // 該当するデータを上書きして保存
+      $profile->fill($profile_form)->save();
+      
+      return redirect('admin/profile');
+      
+  }
+  
 }
